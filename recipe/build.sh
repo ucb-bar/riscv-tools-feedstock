1e82ca254a7d9fb254eb70bea1897f797f2f142b
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2022-08-16 01:56:45 +0000

    Clean up intermediate files after installing

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-NPROC=$CPU_COUNT ./src/build-toolchains.sh --prefix $PREFIX/riscv
+NPROC=$CPU_COUNT ./src/build-toolchains.sh --prefix $PREFIX/riscv --clean-after-install
 
 # create activate & deactivate scripts that manage the toolchain
 mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d
