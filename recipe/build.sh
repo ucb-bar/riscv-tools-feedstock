917b56ef1a78871be22283ecdb01658ac11cbe61
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2022-09-09 16:52:09 +0000

    Add RPATH comment | Update shell pipeline

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
+TOOLCHAIN_NAME=riscv-tools
+
 # strip debugging info
 export LDFLAGS="$LDFLAGS -s"
 
 # de-init unneeded qemu submodule
 git -C ./riscv-gnu-toolchain submodule deinit --force qemu
 
-NPROC=$CPU_COUNT ./build-toolchains.sh --prefix $PREFIX/riscv-tools --clean-after-install
+NPROC=$CPU_COUNT ./build-toolchains.sh --prefix $PREFIX/$TOOLCHAIN_NAME --clean-after-install
 
 # create activate & deactivate scripts that manage the toolchain
 mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d
@@ -16,10 +18,14 @@ mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d
 perl -pe 's/\@NATURE\@/activate/' "${RECIPE_DIR}"/activate.sh > "${PREFIX}"/etc/conda/activate.d/activate-${PKG_NAME}.sh
 perl -pe 's/\@NATURE\@/deactivate/' "${RECIPE_DIR}"/activate.sh > "${PREFIX}"/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
 
-pushd $PREFIX/riscv-tools/sysroot
+pushd $PREFIX/$TOOLCHAIN_NAME/sysroot
 
-for file in $(find . -exec file {} \; | grep -I ELF | awk {'print $1}' | sed 's/.$//')
+# Strip $ORIGIN/.*/lib (if it exists) from the RPATH of all sysroot binaries.
+# Fixes linux boot (since the RPATH shouldn't be set for the sysroot ld*.so)
+shopt -s globstar
+for file in ** ;
 do
+    file -b "${file}" | grep -q 'ELF' || continue
     if output=$(patchelf --print-rpath $file); then
         echo "Current RPATH=$output for FILE=$file"
         if [[ $output == *":"* ]]; then
